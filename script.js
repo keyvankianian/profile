49783cc82c331f25751b955227df4f6e2f09adf4
keyvankianian <kianian.keyvan@email.com>
2025-07-05 22:50:06 +0200

    added one more tab for pictures

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const tabs = ['interviews', 'performances', 'efter2023', 'important'];
+  const tabs = ['artiklar','bilder', 'performances', 'efter2023', 'important'];
   const activeTab = localStorage.getItem('activeTab') || 'important';
 
   document.querySelector(`.tablink[data-tab="${activeTab}"]`)?.click();
